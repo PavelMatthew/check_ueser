be823898aaf403e97765fa7b8475cf4820120980
Pavel Matveev <MaTV5414@yandex.ru>
2022-05-12 14:59:00 +0300

    Update server.bat

--- server.bat
+++ server.bat
@@ -2,4 +2,4 @@ cmd /c "chcp 65001 >nul"
 cmd /c "mkdir "C:\Test"
 cmd /c "net user>"C:\Test\list.txt""
 start http://localhost:3000/checkuser
-cmd /c "node C:\Users\pavelomatthew\Desktop\Checking_of_users\app.js"
+cmd /c "node app.js"
